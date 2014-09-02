67ce7783e368279738f610b0c4a17e725e4861fc
Brian Arenz <brian.arenz@iostudio.com>
2014-09-02 10:34:44 -0500

    Add basic menu

--- baseballtracker.rb
+++ baseballtracker.rb
@@ -14,5 +14,26 @@ until command == 0
   header_main
   menu_main
   command = gets.to_i
+  case command
+  when 1
+    header_main
+    menu_improved_average
+  when 2
+    header_main
+    menu_slugging_percentage
+  when 3
+    input = ""
+    until input.upcase == "CANCEL"
+      header_main
+      menu_import_players
+      input = gets.chomp  
+    end
+  when 4
+    input = ""
+    until input == "cancel"
+      header_main
+      menu_import_stats
+    end
+  end
 end
 clear_screen
--- views/menus.rb
+++ views/menus.rb
@@ -1,5 +1,18 @@
 def menu_main
   menu = "\nMAIN MENU > "
+  menu << green("1")+"(Get Most Improved Batting Average)\s"
+  menu << green("2")+"(Get Team Slugging Percentage)\s"
+  menu << green("3")+"(Import Player Data)\s"
+  menu << green("4")+"(Import Batting Stats)\s"
   menu << red("0")+"(Quit)\n\n"
+  puts menu
+end
+
+def menu_import_players
+  menu = "\nIMPORT PLAYERS > "
+  menu << green("Enter Path to CSV File")
+  menu << "\s\s(Type '"
+  menu << red("CANCEL")
+  menu << "' to exit)"
   puts menu
 end
